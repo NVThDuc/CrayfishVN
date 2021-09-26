3638d89dd972d63b1c406776ff9fc8bcfe213cab
dinhhungGM <dinhhungGM@gmail.com>
2021-09-26 06:10:05 -0400

    fix bug

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const express = require('express')
+
+const app = express()
+const handlebars = require('express-handlebars');
+const port = 3000;
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(express.urlencoded({
+  extended :true
+}));
+app.use(express.json());
+
+// Template
+app.engine('hbs', handlebars(
+  {extname:'.hbs'}
+));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+console.log('PATH: ',path.join(__dirname, 'resources/views') )
+
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+app.get('/Mua-Ban', (req, res) => {
+  res.render('shop');
+})
+
+app.get('/search', (req, res) => {
+  res.render('search');
+})
+
+app.post('/search', (req, res) => {
+  console.log(req.query.body);
+  res.send('');
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
